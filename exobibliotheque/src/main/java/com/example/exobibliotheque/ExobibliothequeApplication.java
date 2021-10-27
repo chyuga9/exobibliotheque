d601d26155111a6c32e02c199c5713258e980a87
Cedric <cedric.campagna@gmail.com>
2021-10-27 14:48:03 +0200

    Ajout d'un logger

--- exobibliotheque/src/main/java/com/example/exobibliotheque/ExobibliothequeApplication.java
+++ exobibliotheque/src/main/java/com/example/exobibliotheque/ExobibliothequeApplication.java
@@ -1,13 +1,23 @@
 package com.example.exobibliotheque;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.log4j.Log4j2;
+
+
 @SpringBootApplication
+@Log4j2
 public class ExobibliothequeApplication {
 
+	private static final Logger logger = LogManager.getLogger(ExobibliothequeApplication.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ExobibliothequeApplication.class, args);
+		logger.info("Démarrage de l'api");
 	}
 
 }
