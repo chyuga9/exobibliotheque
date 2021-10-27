16a9e41cce48ccc4a0d17d3c3180a9a4a609688b
Cedric <cedric.campagna@gmail.com>
2021-10-27 11:34:21 +0200

    base projet

---
+++ exobibliotheque/src/main/java/com/example/exobibliotheque/ExobibliothequeApplication.java
@@ -0,0 +1,13 @@
+package com.example.exobibliotheque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExobibliothequeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExobibliothequeApplication.class, args);
+	}
+
+}
---
+++ exobibliotheque/src/test/java/com/example/exobibliotheque/ExobibliothequeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exobibliotheque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExobibliothequeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
