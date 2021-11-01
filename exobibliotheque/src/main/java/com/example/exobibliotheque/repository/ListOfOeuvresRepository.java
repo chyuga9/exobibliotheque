d636a99423355623f9bb8d24ece8b60410d04f7f
Cedric <cedric.campagna@gmail.com>
2021-11-01 17:19:29 +0100

    Modification des queries

--- exobibliotheque/src/main/java/com/example/exobibliotheque/repository/ListOfOeuvresRepository.java
+++ exobibliotheque/src/main/java/com/example/exobibliotheque/repository/ListOfOeuvresRepository.java
@@ -1,5 +1,7 @@
 package com.example.exobibliotheque.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,8 +11,8 @@ import com.example.exobibliotheque.model.ListOfOeuvresId;
 @Repository
 public interface ListOfOeuvresRepository extends CrudRepository<ListOfOeuvres, ListOfOeuvresId> {
 
-	Iterable<ListOfOeuvres> findByIdUserid(String userid);
-	Iterable<ListOfOeuvres> findByIdName(String name);
+	Iterable<ListOfOeuvres> findByIdUserId(String userid);
+	Optional<ListOfOeuvres> findByIdName(String name);
 
 
 	
