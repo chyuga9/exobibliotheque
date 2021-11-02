ff674b2f731dee2b4eecb9b896b7641d9fa2796b
Cedric <cedric.campagna@gmail.com>
2021-11-02 16:49:38 +0100

    Ajout @CrossOrigin pour pouvoir communiquerr avec Angular

--- exobibliotheque/src/main/java/com/example/exobibliotheque/controller/OeuvreController.java
+++ exobibliotheque/src/main/java/com/example/exobibliotheque/controller/OeuvreController.java
@@ -6,6 +6,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,6 +31,7 @@ public class OeuvreController {
 	@Autowired
 	private OeuvreService oeuvreService;
 	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@GetMapping("/oeuvres")
 	public ResponseEntity<Iterable<Oeuvre>> getOeuvres(){
 		logger.info("Controller - Recherche de toutes les oeuvres");
