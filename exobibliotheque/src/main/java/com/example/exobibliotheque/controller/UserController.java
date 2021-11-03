e8847609e6958a1a3870b9a567c7e52bfd0a185b
Cedric <cedric.campagna@gmail.com>
2021-11-03 10:32:34 +0100

    Ajout du reste du CRUD

--- exobibliotheque/src/main/java/com/example/exobibliotheque/controller/UserController.java
+++ exobibliotheque/src/main/java/com/example/exobibliotheque/controller/UserController.java
@@ -6,14 +6,21 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.exobibliotheque.ExobibliothequeApplication;
+import com.example.exobibliotheque.model.ListOfOeuvres;
+import com.example.exobibliotheque.model.ListOfOeuvresId;
+import com.example.exobibliotheque.model.Oeuvre;
 import com.example.exobibliotheque.model.User;
 import com.example.exobibliotheque.service.UserService;
 
@@ -32,6 +39,12 @@ public class UserController {
 		return ResponseEntity.ok().body(userService.getUsers());
 	}
 	
+	@GetMapping("/user/{id}")
+	public ResponseEntity<User> getSingleUser(@PathVariable int userId){
+		logger.info("Controller - Recherche de l'utilisateur "+ userId);
+		return ResponseEntity.ok().body(userService.getSingleUser(userId));
+	}
+	
 	@PostMapping("/user")
 	public ResponseEntity<User> createUser(@RequestBody User user){
 		logger.info("Controller - Création d'un nouveau user");
@@ -46,5 +59,19 @@ public class UserController {
 	
 		return ResponseEntity.created(location).body(newUser);
 
+	}
+	
+	@PutMapping("/user/{id}")
+	public ResponseEntity<User> updateOeuvre(@RequestBody User user){
+		logger.info("Controller - Mise à jour d'une utilisateur");
+		User updatedUser = userService.updateUser(user);
+		return ResponseEntity.ok().body(updatedUser);
+	}
+	
+	@DeleteMapping("/user/{id}")
+	public ResponseEntity<String> deleteUser(@PathVariable int userId){
+		logger.info("Controller - Recherche de l'utilisateur avec l'id \""+ userId + "\" pour suppression");
+		userService.deleteUser(userId);
+		return ResponseEntity.ok().body("User deleted");
 	}
 }
--- exobibliotheque/src/main/java/com/example/exobibliotheque/service/UserService.java
+++ exobibliotheque/src/main/java/com/example/exobibliotheque/service/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.exobibliotheque.ExobibliothequeApplication;
+import com.example.exobibliotheque.model.Oeuvre;
 import com.example.exobibliotheque.model.User;
 import com.example.exobibliotheque.repository.UserRepository;
 
@@ -13,17 +14,32 @@ import com.example.exobibliotheque.repository.UserRepository;
 public class UserService {
 
 	private static final Logger logger = LogManager.getLogger(ExobibliothequeApplication.class);
-	
+
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	public Iterable<User> getUsers() {
 		logger.info("Service - Recherche tous les users");
 		return userRepository.findAll();
 	}
-	
+
+	public User getSingleUser(int userId) {
+		if (!userRepository.existsById(userId))
+			logger.info("Service - L'utilisateur avec l'id \"" + userId + "\" n'existe pas");
+		return userRepository.findById(userId).get();
+	}
+
 	public User createUser(User user) {
 		logger.info("Service - Création d'un nouvel utilisateur");
+		return userRepository.save(user);
+	}
+
+	public void deleteUser(int userId) {
+		userRepository.deleteById(userId);
+
+	}
+
+	public User updateUser(User user) {
 		return userRepository.save(user);
 	}
 
