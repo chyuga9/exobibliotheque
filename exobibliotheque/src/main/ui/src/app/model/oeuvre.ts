b35559b32d1b00bf9db56374cd5a8c65fd82d515
Cedric <cedric.campagna@gmail.com>
2022-02-21 12:51:19 +0100

    Added comments to explain decision to not choose interface instead of class

--- exobibliotheque/src/main/ui/src/app/model/oeuvre.ts
+++ exobibliotheque/src/main/ui/src/app/model/oeuvre.ts
@@ -4,8 +4,18 @@ import { Genre } from "./genre";
 export class Oeuvre {
     
             id:number;
- 
+            /*
+            // Ne peut pas transformer la classe en interface car un moment je crée un objet
+            // Oeuvre pour l'envoyer dans l'api Java
+            name:String;
+            dateDeSortie: Date;
+            synopsis: string;
+            category:Category;
+            genres:Genre[];
+            image?:string;
+            */
+
             // vient de l'ancienne classe que j'ai transformé en interface
-            constructor(public name:String, public dateDeSortie: Date, public synopsis: string, public category:Category, public genres:Genre[]){}
+            constructor(public name:String, public dateDeSortie: Date, public synopsis: string, public category:Category, public genres:Genre[], public image?:string){}
             // constructor(private name:String, private dateDeSortie: Date, private synopsis: string){}
          }
