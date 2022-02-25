dee20a6750b6c1db8e5b8905c8a5cca4a2bb363d
Cedric <cedric.campagna@gmail.com>
2022-02-25 09:48:25 +0100

    Endpoints passed to "permit at all" : "/genres", "genres/*" et "categories"

--- exobibliotheque/src/main/java/com/example/exobibliotheque/configuration/SpringSecurityConfig.java
+++ exobibliotheque/src/main/java/com/example/exobibliotheque/configuration/SpringSecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder.BCryptVersion;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -24,146 +25,160 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
-	
+
 	@Autowired
 	DataSource datasource;
-	
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
-		auth.jdbcAuthentication()
-		.dataSource(datasource)
-		// Personnalisation des requêtes 
-		.usersByUsernameQuery("select email, password, enabled "
-				+ "from user "
-				+"where email = ?")
-		.authoritiesByUsernameQuery("select email, authority "
-				+ "from authority "
-				+"where email = ?");
 
-	}
+
+@Override
+protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+	auth.jdbcAuthentication()
+	.dataSource(datasource).passwordEncoder(getPasswordEncoder())
+	// Personnalisation des requêtes 
+	.usersByUsernameQuery("SELECT email, password, enabled "
+			+ "FROM user "
+			+"WHERE email = ?")
+	.authoritiesByUsernameQuery("SELECT email, authority "
+			+ "FROM authority "
+			+"WHERE email = ?");
+
+}
+
+
+
+@Bean
+public PasswordEncoder getPasswordEncoder() {
+	return new BCryptPasswordEncoder(12) ; //NoOpPasswordEncoder.getInstance()
+}
 	
-	@Override
-	public void configure(HttpSecurity http) throws Exception{
-		
-		// !!! Permet d'utiliser presque toutes les requetes HTTP, csrf().disable() permet de desactiver la securité CORS
-		/*
-        http.authorizeRequests().antMatchers("/").permitAll();
-		http.csrf().disable();
-		*/
-		
-		http
-			.cors()
-		.and()
-			.httpBasic()
-			.and()
-			.authorizeRequests()
-	  			.antMatchers("/admin").hasRole("ADMIN")
-	  			.antMatchers("/user").hasAnyRole("ADMIN","USER")
-	  			.antMatchers("/api/login").permitAll()
-	  			.antMatchers("/api/oeuvres").permitAll() // permet à tout le monde de faire toutes les requetes...il faudrait créer un nouvel pour le post peut etre
-	  			.antMatchers("/api/createuser").permitAll()
-	  			.antMatchers("/api/genres").hasAnyRole("ADMIN","USER")
-	  			//.antMatchers("/api/genres").permitAll()
-	  			.antMatchers("/api/categories").hasAnyRole("ADMIN","USER")
-	  		//	.antMatchers("/api/**").permitAll()// accepte tous les utilisateurs
-	  			.anyRequest().authenticated() 
-		.and()
-	  		.formLogin()
-		.and()
-	  		.logout()
-	  			.permitAll();
-		
-	String[] tab1 = {"012","123","234"};
-	ArrayList<String> tab2;
+@Override
+public void configure(HttpSecurity http) throws Exception{
 	
-	//.and().csrf().disable().httpBasic();
-	//.and().formLogin();
-			
-			/*
-			 * Permet de contourner l'erreur 403 obtenu pour la requete POST
-			 * à creuser
-			 */
-		 // !!! Permet d'utiliser POST / DELETE
-		// .csrf().disable();
-			//.and()
-			//.oauth2Login();
-	}
+	// !!! Permet d'utiliser presque toutes les requetes HTTP, csrf().disable() permet de desactiver la securité CORS
 	/*
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+    http.authorizeRequests().antMatchers("/").permitAll();
+	http.csrf().disable();
 	*/
 	
-	@Bean
-	public PasswordEncoder getPasswordEncoder() {
-		return NoOpPasswordEncoder.getInstance();
-	}
+	// ordre antMatchers() -> du plus spécifique au plus général
+	//https://www.baeldung.com/spring-security-login
 	
-	/*
-	 * 
-	 *  ----------  HASH PASSWORD
-	 *  
-	Option 1 - n'est pas considéré comme sécurisé pour un environnement de production, il est préférable hasher le password à l'extérieur :
+http
+		.cors();
+	http
+		.httpBasic()
+		.and()
+		.authorizeRequests()
+  			.antMatchers("/admin").hasRole("ADMIN")
+  			.antMatchers("/user").hasAnyRole("ADMIN","USER")
+  			.antMatchers("/api/login").permitAll().antMatchers("/api/logemail").permitAll()
+  			.antMatchers("/api/oeuvres").permitAll() // permet à tout le monde de faire toutes les requetes...il faudrait créer un nouvel pour le post peut etre
+  			.antMatchers("/api/createuser").permitAll()
+  			//.antMatchers("/api/genres").hasAnyRole("ADMIN","USER")
+  			.antMatchers("/api/genres").permitAll()
+  			.antMatchers("/api/genres/*").permitAll()
+  			//.antMatchers("/api/categories").hasAnyRole("ADMIN","USER")
+  			.antMatchers("/api/categories").permitAll()
+  		//	.antMatchers("/api/**").permitAll()// accepte tous les utilisateurs
+  			.anyRequest().authenticated() 
+	.and()
+  		.formLogin()
+  		.loginPage("http://localhost:4200/signin")
+  		.permitAll()
+	.and()
+  		.logout();
+  		
+  			//.permitAll();
+}
 
-	User user = User.withDefaultPasswordEncoder()
-	  .username("user")
-	  .password("password")
-	  .roles("user")
-	  .build();
-	System.out.println(user.getPassword());
-	// {bcrypt}$2a$10$dXJ3SW6G7P50lGmMkkmwe.20cQQubK3.HZWzG3YB1tlRy.fqvM/BG
+//.and().csrf().disable().httpBasic();
+//.and().formLogin();
+		
+		/*
+		 * Permet de contourner l'erreur 403 obtenu pour la requete POST
+		 * à creuser
+		 */
+	 // !!! Permet d'utiliser POST / DELETE
+	// .csrf().disable();
+		//.and()
+		//.oauth2Login();
 
-	Option 2 - n'est pas considéré comme sécurisé pour un environnement de production, il est préférable d'encoder / hasher le password à l'extérieur:
+/*
+@Bean
+public PasswordEncoder passwordEncoder() {
+	return new BCryptPasswordEncoder();
+}
+*/
+/*
+@Bean
+public PasswordEncoder getPasswordEncoder() {
+	return NoOpPasswordEncoder.getInstance();
+}
 
-	UserBuilder users = User.withDefaultPasswordEncoder();
-	User user = users
-	  .username("user")
-	  .password("password")
-	  .roles("USER")
-	  .build();
-	User admin = users
-	  .username("admin")
-	  .password("password")
-	  .roles("USER","ADMIN")
-	  .build();
-	  
-	  
-	  ----------  ENCODE PASSWORD
+
+ * 
+ *  ----------  HASH PASSWORD
+ *  
+Option 1 - n'est pas considéré comme sécurisé pour un environnement de production, il est préférable hasher le password à l'extérieur :
+
+User user = User.withDefaultPasswordEncoder()
+  .username("user")
+  .password("password")
+  .roles("user")
+  .build();
+System.out.println(user.getPassword());
+// {bcrypt}$2a$10$dXJ3SW6G7P50lGmMkkmwe.20cQQubK3.HZWzG3YB1tlRy.fqvM/BG
+
+Option 2 - n'est pas considéré comme sécurisé pour un environnement de production, il est préférable d'encoder / hasher le password à l'extérieur:
+
+UserBuilder users = User.withDefaultPasswordEncoder();
+User user = users
+  .username("user")
+  .password("password")
+  .roles("USER")
+  .build();
+User admin = users
+  .username("admin")
+  .password("password")
+  .roles("USER","ADMIN")
+  .build();
+  
+  
+  ----------  ENCODE PASSWORD
 
 
 https://docs.spring.io/spring-security/site/docs/current/reference/html5
 
-	  Option 1 : 
-	  
-	  spring encodepassword password
+  Option 1 : 
+  
+  spring encodepassword password
 {bcrypt}$2a$10$X5wFBtLrL/kHcmrOGGTrGufsBX8CJ0WpQpF3pgeuxBB/H73BK1DW6
 
 
-	Option 2 : 
-	
-// Create an encoder with strength 16
+Option 2 : 
+
+//Create an encoder with strength 16
 BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(16);
 String result = encoder.encode("myPassword");
 assertTrue(encoder.matches("myPassword", result));
 
-	Option 3 - The current implementation of the Argon2PasswordEncoder requires BouncyCastle:
-	
-	// Create an encoder with all the defaults
+Option 3 - The current implementation of the Argon2PasswordEncoder requires BouncyCastle:
+
+// Create an encoder with all the defaults
 Argon2PasswordEncoder encoder = new Argon2PasswordEncoder();
 String result = encoder.encode("myPassword");
 assertTrue(encoder.matches("myPassword", result));
 
-	Option 4 - when FIP certification is required :
-	
-	// Create an encoder with all the defaults
+Option 4 - when FIP certification is required :
+
+// Create an encoder with all the defaults
 Pbkdf2PasswordEncoder encoder = new Pbkdf2PasswordEncoder();
 String result = encoder.encode("myPassword");
 assertTrue(encoder.matches("myPassword", result));
 
-	Option 5 :
-	
-	// Create an encoder with all the defaults
+Option 5 :
+
+// Create an encoder with all the defaults
 SCryptPasswordEncoder encoder = new SCryptPasswordEncoder();
 String result = encoder.encode("myPassword");
 assertTrue(encoder.matches("myPassword", result));
@@ -172,22 +187,24 @@ assertTrue(encoder.matches("myPassword", result));
 https://docs.spring.io/spring-security/site/docs/current/reference/html5/#headers
 
 ------- Response header
-	*/
-	
-	//   aide spring pour le CORS --> https://spring.io/blog/2015/06/08/cors-support-in-spring-framework
-	@Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/api/**")
-        //registry.addMapping("/**")
-			.allowedOrigins("http://localhost:4200/")
-			.allowedMethods("PUT", "DELETE", "POST")
-				.allowedHeaders("header1", "header2", "header3")
-			.exposedHeaders("header1", "header2")
-			.allowCredentials(false).maxAge(3600);
-        };
-        };
-	}
+*/
+
+//   aide spring pour le CORS --> https://spring.io/blog/2015/06/08/cors-support-in-spring-framework
+/*
+@Bean
+public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurerAdapter() {
+        @Override
+public void addCorsMappings(CorsRegistry registry) {
+	registry.addMapping("/api/**")
+    //registry.addMapping("/**")
+		.allowedOrigins("http://localhost:4200/")
+		.allowedMethods("PUT", "DELETE", "POST")
+			.allowedHeaders("header1", "header2", "header3")
+		.exposedHeaders("header1", "header2")
+		.allowCredentials(false).maxAge(3600);
+    };
+    };
+}
+*/
 }
